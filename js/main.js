c16d09bb787536ea8245d148b265befba1a1beaf
Miriam <miriam.bonninghoff@dci-student.org>
2023-11-06 19:58:37 +0100

    main.js: klassen hinzugefügt img-size show hide index.html angepasst

--- js/main.js
+++ js/main.js
@@ -1,44 +1,44 @@
-/* Warten auf das Laden der Webseite */
 document.addEventListener("DOMContentLoaded", function() {
-    // Start-Button selektieren
     const startButton = document.getElementById("start-button");
-  
-    // Charaktererstellungselement selektieren
     const characterCreation = document.getElementById("char-creation");
+    const playerLogo = document.getElementById("player-logo");
+    const playerImage = document.getElementById("player-image");
   
-    // Hintergrundbild zu Char-Erstellung ändern
     function changeBackground() {
       const backgroundImage = document.getElementById("background-image");
       backgroundImage.src = "img/char-creation.jpg";
     }
-    // H1-Text ändern
-    function changeText() {
-        const h1 = document.querySelector(".text-border-lightgrey");
-        h1.innerHTML = "Charaktererstellung";
-    }
-    //Willkommens-Text ausblenden
-    function hideText() {
-    //   const h1 = document.querySelector(".text-size-50"); // Hier den richtigen Selektor verwenden
-      const welcomeText = document.querySelector(".text-size-40");
-      const buttonContainer = document.querySelector(".button-start-screen");
-    //   h1.style.display = "none";
-      welcomeText.style.display = "none";
-      buttonContainer.style.display = "none";
+  
+    function hide(element) {
+      element.style.display = "none";
     }
   
-    // Char-Erstellung beim Start verstecken
+    function show(element) {
+      element.style.display = "block";
+    }
+  
+    function imgSize(element, size) {
+      element.style.width = size;
+    }
+  
     characterCreation.style.display = "none";
   
-    // Start-Button Klick-Event
     startButton.addEventListener("click", function() {
-      // Char-Erstellung anzeigen lassen
       characterCreation.style.display = "block";
-      // Hintergrundbild ändern
       changeBackground();
-    //  Willkommens-Text ausblenden
-      hideText();
-    //   H1-Text ändern
-        changeText();
+      hide(document.querySelector(".text-size-40"));
+      hide(document.querySelector(".button-start-screen"));
+      hide(document.querySelector("#logo"));
+      imgSize(playerLogo, "15%");
+      playerLogo.style.position = "absolute";
+      playerLogo.style.top = "0";
+      playerLogo.style.left = "0";
+      imgSize(playerImage, "20%");
+    });
+  
+    const selectImageButton = document.getElementById("select-image-button");
+    selectImageButton.addEventListener("click", function() {
+      // Hier kannst du die Logik hinzufügen, um ein Bild auszuwählen
     });
   });
   
