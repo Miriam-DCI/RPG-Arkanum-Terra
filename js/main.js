76af96d6a6da42832e2514f619f936edc1fa2d6e
Miriam <miriam.bonninghoff@dci-student.org>
2023-11-07 00:45:56 +0100

    char erstellung design technisch fast fertig funktionen und finetuning fehlen noch responsiv design kommt als nächstes

--- js/main.js
+++ js/main.js
@@ -1,44 +1,42 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const startButton = document.getElementById("start-button");
-    const characterCreation = document.getElementById("char-creation");
-    const playerLogo = document.getElementById("player-logo");
-    const playerImage = document.getElementById("player-image");
-  
-    function changeBackground() {
-      const backgroundImage = document.getElementById("background-image");
-      backgroundImage.src = "img/char-creation.jpg";
-    }
-  
-    function hide(element) {
-      element.style.display = "none";
-    }
-  
-    function show(element) {
-      element.style.display = "block";
-    }
-  
-    function imgSize(element, size) {
-      element.style.width = size;
-    }
-  
-    characterCreation.style.display = "none";
-  
+  const startButton = document.getElementById("start-button");
+  const characterCreation = document.getElementById("char-creation");
+  const playerLogo = document.getElementById("player-logo");
+
+  function changeBackground() {
+    const backgroundImage = document.getElementById("background-image");
+    backgroundImage.src = "img/char-creation.jpg";
+  }
+
+  function hide(element) {
+    element.style.display = "none";
+  }
+
+  function show(element) {
+    element.style.display = "block";
+  }
+
+  function imgSize(element, size) {
+    element.style.width = size;
+  }
+
+  characterCreation.style.display = "none";
+
+  if (startButton) {
     startButton.addEventListener("click", function() {
-      characterCreation.style.display = "block";
+      characterCreation.style.display = "grid";
       changeBackground();
       hide(document.querySelector(".text-size-40"));
       hide(document.querySelector(".button-start-screen"));
       hide(document.querySelector("#logo"));
-      imgSize(playerLogo, "15%");
-      playerLogo.style.position = "absolute";
-      playerLogo.style.top = "0";
-      playerLogo.style.left = "0";
-      imgSize(playerImage, "20%");
+      imgSize(playerLogo, "15%"); // Änderung hier
     });
-  
-    const selectImageButton = document.getElementById("select-image-button");
+  }
+
+  const selectImageButton = document.getElementById("select-image-button");
+  if (selectImageButton) {
     selectImageButton.addEventListener("click", function() {
       // Hier kannst du die Logik hinzufügen, um ein Bild auszuwählen
     });
-  });
-  
+  }
+});
