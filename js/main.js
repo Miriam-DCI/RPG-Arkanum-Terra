819ad804c3dc6f73e65cd9b92bea9cf53bff4647
Miriam <miriam.bonninghoff@dci-student.org>
2023-11-07 23:52:25 +0100

    ach lassen wir das und machen erstmal mit dem spaßigen teil weiter

--- js/main.js
+++ js/main.js
@@ -2,27 +2,29 @@ document.addEventListener("DOMContentLoaded", function() {
     const startButton = document.getElementById("start-button");
     const charSelection = document.getElementById("char-selection");
     const titleScreen = document.getElementById("title-screen");
-
-    function changeBackground(newImage) {
-        const gameScreen = document.getElementById("game-window");
-        gameScreen.style.backgroundImage = `url(${newImage})`;
+  
+    function changeBackground(oldImage, newImage) {
+      const backgroundImage = document.getElementById(oldImage);
+      backgroundImage.src = newImage;
     }
+  
     function hide(element) {
-        element.style.display = "none";
+      element.style.display = "none";
     }
-
+  
     function show(element) {
-        element.style.display = "block";
+      element.style.display = "grid"; // Ändere display auf "grid" für die Charakterauswahl
     }
-
+  
     charSelection.style.display = "none";
-
+  
     if (startButton) {
-        startButton.addEventListener("click", function() {
-            charSelection.style.display = "grid";
-            hide(titleScreen);
-            // changeBackground("img/char-creation.jpg");
-            show(charSelection);
-        });
+      startButton.addEventListener("click", function() {
+        charSelection.style.display = "grid";
+        hide(titleScreen);
+        changeBackground("background-image", "img/char-creation.jpg"); // Ändere das Hintergrundbild für die Charakterauswahl
+        show(charSelection);
+      });
     }
-});
+  });
+  
