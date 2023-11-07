646d80b6824bdeddbda759a282508ede5b2f94ae
Miriam <miriam.bonninghoff@dci-student.org>
2023-11-07 18:50:00 +0100

    code aufgeräumt char creation angepasst neue klassen für transparenz hinzugefügt js funktionen überarbeitet positionierung muss noch überarbeitet werden

--- js/main.js
+++ js/main.js
@@ -1,28 +1,28 @@
 document.addEventListener("DOMContentLoaded", function() {
-  const startButton = document.getElementById("start-button");
-  const charSelection = document.getElementById("char-selection");
-  const titleScreen = document.getElementById("title-screen");
+    const startButton = document.getElementById("start-button");
+    const charSelection = document.getElementById("char-selection");
+    const titleScreen = document.getElementById("title-screen");
 
-  function changeBackground() {
-      const backgroundImage = document.getElementById("background-image");
-      backgroundImage.src = "img/char-creation.jpg";
-  }
+    function changeBackground(newImage) {
+        const gameScreen = document.getElementById("game-window");
+        gameScreen.style.backgroundImage = `url(${newImage})`;
+    }
+    function hide(element) {
+        element.style.display = "none";
+    }
 
-  function hide(element) {
-      element.style.display = "none";
-  }
+    function show(element) {
+        element.style.display = "block";
+    }
 
-  function show(element) {
-      element.style.display = "block";
-  }
+    charSelection.style.display = "none";
 
-  charSelection.style.display = "none";
-
-  if (startButton) {
-      startButton.addEventListener("click", function() {
-          charSelection.style.display = "block";
-          changeBackground();
-          hide(titleScreen);
-      });
-  }
+    if (startButton) {
+        startButton.addEventListener("click", function() {
+            charSelection.style.display = "grid";
+            hide(titleScreen);
+            // changeBackground("img/char-creation.jpg");
+            show(charSelection);
+        });
+    }
 });
