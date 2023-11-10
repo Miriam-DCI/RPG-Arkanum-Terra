883e02c44f9e9787565d553811e5b3860b92002e
Miriam <miriam.bonninghoff@dci-student.org>
2023-11-10 15:02:34 +0100

    arbeite an export und import noch nicht ganz fertig

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ tutorial video:https://www.youtube.com/watch?v=R1S_NhKkvGA&t=619s
 zum verpixeln von bildern: https://giventofly.github.io/pixelit/#tryit
 
 honorable mentions: melle mit styling ideen
+Patrick und Nadja fürs alpha testsen und feedback (den walt gibt es nicht er ist eine lüge sowie id 4)
 
 erstellung von pixelart: https://www.pixilart.com/draw
 
@@ -14,6 +15,37 @@ strg+p datei suche in repo
 strg+f suche in datei
 strg+d mehrfach suche in datei
 strg+g zeilen nummer
+
+Grundlegende Struktur:
+
+Die Geschichte ist in einem Array namens textNodes organisiert.
+Jeder Textknoten (Text-Node) im Array hat eine eindeutige ID, Text, und Optionen für den Spieler, die zu anderen Textknoten führen.
+Spiel-Start und GUI-Elemente:
+
+Es gibt Funktionen wie startGame, showtextNode, und selectOption, die das Spiel starten, Texte anzeigen und Optionen auswählen.
+Das Spiel wird durch Klicken auf Buttons gesteuert, die dem Spieler Optionen bieten.
+Inventar und Hintergrundbild:
+
+Ein Inventarobjekt und ein Hintergrundbild (über das Ändern des Hintergrundbilds) wurden implementiert, um das Spielererlebnis zu erweitern.
+Charakterfortschritt und Funktionen:
+
+Es gibt Funktionen, um den Fortschritt des Charakters zu verfolgen, wie addAlly, addEnemy, updateTimeOfDay, adjustMorality, triggerFlashback, improveSkills, und addItemToInventory.
+Interaktive Elemente:
+
+Die Geschichte enthält interaktive Elemente wie das Finden von Schlüsseln, das Berühren von Kristallen, das Treffen von Verbündeten und Feinden, das Entdecken von Orten und das Sammeln von Wissen.
+Bedingte Optionen und Fortschritt:
+
+Optionen können bedingt sein, basierend auf dem Inventar des Spielers oder seinem Fortschritt in der Geschichte.
+Audio-Element:
+
+Ein Audioelement wurde eingeführt, um Soundeffekte im Spiel abzuspielen.
+Modularer Aufbau:
+
+Die Geschichte ist modular aufgebaut, um verschiedene Pfade zu ermöglichen, je nach den Entscheidungen des Spielers.
+Integration neuer Funktionen:
+
+Die neuen Funktionen, die wir eingeführt haben, wie Verbündete und Feinde, geheime Orte, zeitliche Elemente, Moralkonflikte, Rückblenden und Charakterentwicklung, können nun sinnvoll in der Story verwendet werden.
+Code-Kommentare:
 
 # RPG-Arkanum-Terra
 
---
+++ data/game.js
@@ -0,0 +1,122 @@
+/*
+  TODO:
+  funktion zum tausch des bg-img hintergrundbilder funktionieren aktuell nicht weil DOM fehlt
+  array aus dateipfade die sich je nach text-id anpassen 
+  
+  
+  item abfrage überarbeiten
+  Story weiter schreiben (ausführlicher aktuell sind nur kern punkte vorhanden)
+
+  array für charackter story fortschritt (ids der textnodes werden ins array
+  gepuscht und später kann abgefragt werden ob wir bereites an einem bestimmten 
+  story punkt waren um andeere optionen zu bekommen)
+*/
+
+// imports
+
+import { textNodes, character } from "./textNodes.js";
+
+const textElement = document.getElementById("text");
+const buttonOptionsElement = document.getElementById("btn-options");
+const audioElement = document.getElementById("audio");
+
+let inventory = {};
+const bgImgPathArray = ["/img/Pfad-zur-burg.jpg", "/img/door-closeUp.jpg"];
+
+function setBgImg(imgUrlIndex) {
+  const consoleElement = document.getElementById("console");
+  consoleElement.style.backgroundImage = `url(${bgImgPathArray[imgUrlIndex]})`;
+}
+
+function addAlly(ally) {
+  character.allies.push(ally);
+}
+
+function addEnemy(enemy) {
+  character.enemies.push(enemy);
+}
+
+function discoverLocation(location) {
+  if (!character.discoveredLocations.includes(location)) {
+    character.discoveredLocations.push(location);
+  }
+}
+
+function updateTimeOfDay() {
+  // Logik zur Aktualisierung der Tageszeit
+}
+
+function adjustMorality(points) {
+  character.moralityPoints += points;
+}
+
+function triggerFlashback() {
+  character.flashbackTriggered = true;
+}
+
+function improveSkills(skillType) {
+  if (!character.characterSkills.includes(skillType)) {
+    character.characterSkills.push(skillType);
+  }
+}
+
+function addItemToInventory(item) {
+  character.inventory.push(item);
+}
+
+function startGame() {
+  inventory = {};
+  playSound();
+  showtextNode(1);
+}
+function playSound() {
+  audioElement.play();
+}
+
+function showtextNode(textNodeindex) {
+  const textNode = textNodes.find((textNode) => textNode.id === textNodeindex);
+  textElement.innerText = textNode.text;
+  while (buttonOptionsElement.firstChild) {
+    buttonOptionsElement.removeChild(buttonOptionsElement.firstChild);
+  }
+
+  textNode.options.forEach((option) => {
+    if (showOption(option)) {
+      const button = document.createElement("button");
+      button.innerText = option.text;
+      button.classList.add("btn");
+      button.addEventListener("click", () => selectOption(option));
+      buttonOptionsElement.appendChild(button);
+    }
+  });
+}
+
+function showOption(option) {
+  return (
+    option.requiredinventory == null || option.requiredinventory(inventory)
+  );
+}
+
+function selectOption(option) {
+  const nextTextNodeId = option.nextText;
+  inventory = Object.assign(inventory, option.setinventory);
+  showtextNode(nextTextNodeId);
+}
+
+startGame();
+
+export {
+  selectOption,
+  showOption,
+  showtextNode,
+  startGame,
+  addItemToInventory,
+  improveSkills,
+  triggerFlashback,
+  adjustMorality,
+  updateTimeOfDay,
+  discoverLocation,
+  addEnemy,
+  addAlly,
+  setBgImg,
+};
